80c027d56d98f0c1cec7fe001483a01a00ab490f
hexapulsar <magdalenadelafuente@ug.uchile.cl>
2025-06-09 10:19:16 -0400

    "notebooks"

--- pipeline/training/lc_classifier_ztf/ATAT_ALeRCE/src/launchers/launch_LC.sh
+++ pipeline/training/lc_classifier_ztf/ATAT_ALeRCE/src/launchers/launch_LC.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 cd ../../
-export CUDA_VISIBLE_DEVICES=0,3 
+export CUDA_VISIBLE_DEVICES=0,1,2,3 
 
 # Define variables
  
 #!/bin/bash
-
-expname='new_window_dataset_v5'
+seed=0
+expname=weighted_v7
 # Set experiment variables correctly
 export EXPERIMENT_TYPE='LC'
-export EXPERIMENT_NAME=class_${expname}_MLP_1E_trans
-export EXPERIMENT_OUTPUT_PATH="./results/ZTF_ff/LC/$EXPERIMENT_NAME/"
+export EXPERIMENT_NAME=class_${expname}_FL
+export EXPERIMENT_OUTPUT_PATH="./results/200/LC/$EXPERIMENT_NAME/"
 export LOG_FILENAME="$EXPERIMENT_OUTPUT_PATH/$EXPERIMENT_NAME.log"
-export CONFIGS_PATH="/home/mdelafuente/pipeline/pipeline/training/lc_classifier_ztf/ATAT_ALeRCE/src/configs"
+export CONFIGS_PATH="/home/mdelafuente/pipeline/pipeline/training/lc_classifsier_ztf/ATAT_ALeRCE/src/configs"
 
-export CHECKPOINT=/home/mdelafuente/pipeline/pipeline/training/lc_classifier_ztf/ATAT_ALeRCE/results/ZTF_ff/LC/${expname}/
+export CHECKPOINT=/home/mdelafuente/pipeline/pipeline/training/lc_classifier_ztf/ATAT_ALeRCE/results/200/LC/${expname}/
 # Ensure directories exist
 mkdir -p "$EXPERIMENT_OUTPUT_PATH"
 export HYDRA_FULL_ERROR=1
@@ -27,10 +27,12 @@ python training.py \
   ++ATATConfig.log_filename=$LOG_FILENAME \
   ++ATATConfig.save_dir_path=$EXPERIMENT_OUTPUT_PATH\
   ++ATATConfig.datamodule.dataset.experiment_type=$EXPERIMENT_TYPE\
+  ++ATATConfig.datamodule.dataset.seed=$seed\
   ++ATATConfig.loggers.tensorboard.save_dir=$EXPERIMENT_OUTPUT_PATH\
   ++ATATConfig.loggers.csv.save_dir=$EXPERIMENT_OUTPUT_PATH\
   ++ATATConfig.callbacks.model_checkpoint.dirpath=$EXPERIMENT_OUTPUT_PATH\
-  ++ATATConfig.datamodule.train_use_sampler=1\
   hydra.run.dir=$EXPERIMENT_OUTPUT_PATH\
-  ++ATATConfig.checkpoint=$CHECKPOINT
+  #++ATATConfig.lc.checkpoint=$CHECKPOINT
+
+
 
